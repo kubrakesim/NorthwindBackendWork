a24e4bae3b5a21240c896dbfd18d115611ba35c4
kubra <kubrakesim61@gmail.com>
2020-06-03 13:48:05 +0300

    Asp.Net Core 3.1 API Çalışmalarım

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Security.Jwt;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProuctDal>().As<IProductDal>();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,6 @@
+Asp.net Core 3.1 API Çalışmalarım : 
+Katmanlı mimari kullanımı
+Dependencey İnjection çalışmaları,
+İşlem başarı ve hata sisteminin oluşturulması,
+SuccessDataResult Altyapısını kurmak,
+Autofac kurulum  işlemleri üzerindeki mevcut çalışmalarım.
